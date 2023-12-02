e2d694cf80b543e80f53516ae03b934602770992
Flinth <f.houdemer@gmail.com>
2023-12-02 16:25:42 +0100

    :sparkles: Add day2-part1 solution and input

---
+++ day2/part1/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	// Read lines from input.txt.
+	data, err := os.ReadFile("../input.txt")
+	check(err)
+
+	games := strings.Split(string(data), "\n")
+
+	// Array of ids that will match.
+	matches := make([]int, 0)
+
+	redsLimit := 12
+	greensLimit := 13
+	bluesLimit := 14
+
+	re := regexp.MustCompile(`(\d+)`)
+	for i, line := range games {
+		if len(line) == 0 {
+			continue
+		}
+
+		maxReds, maxGreens, maxBlues := 0, 0, 0
+
+		// Parses only after "Game X:" and split by ";"
+		sets := strings.Split(strings.Split(line, ":")[1], ";")
+
+		// Loop through each "set"
+		for _, set := range sets {
+			if len(set) == 0 {
+				continue
+			}
+
+			// Stores each color count value and its start/end indexes.
+			colorsCounts := re.FindAll([]byte(set), -1)
+			colorsIndex := re.FindAllIndex([]byte(set), -1)
+
+			for cIndex, colorsCount := range colorsCounts {
+				count, err := strconv.Atoi(string(colorsCount))
+				check(err)
+
+				// Check first letter of color associated with the cound.
+				// Index is {end-of-color-count-value-match}+1
+				colorFirstLetter := set[colorsIndex[cIndex][1]+1]
+
+				if colorFirstLetter == 'r' && count > maxReds {
+					maxReds = count
+				} else if colorFirstLetter == 'g' && count > maxGreens {
+					maxGreens = count
+				} else if colorFirstLetter == 'b' && count > maxBlues {
+					maxBlues = count
+				}
+			}
+		}
+
+		// Matching ? Let's add !
+		if maxReds <= redsLimit && maxGreens <= greensLimit && maxBlues <= bluesLimit {
+			matches = append(matches, i+1)
+		}
+	}
+
+	result := 0
+
+	for _, match := range matches {
+		result += match
+	}
+
+	fmt.Printf("\nResult: %v\n", result)
+}
